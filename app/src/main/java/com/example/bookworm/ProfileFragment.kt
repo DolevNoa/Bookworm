193c3e15abb36be9148dd1a02e0d5019f9ea22fe
NoaDolev <noa.dolev96@gmail.com>
2024-07-23 23:18:59 +0300

    Utilize ProfileFragment
    This fragment was recreated using Android Studio tools instead of manually creating the class, therefore default params were added.

--- app/src/main/java/com/example/bookworm/ProfileFragment.kt
+++ app/src/main/java/com/example/bookworm/ProfileFragment.kt
@@ -1,4 +1,4 @@
-package com.example.yourapp
+package com.example.bookworm
 
 import android.content.Context
 import android.os.Bundle
@@ -10,15 +10,35 @@ import android.widget.EditText
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
-import com.example.bookworm.R
 
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [ProfileFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
 class ProfileFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
 
     private lateinit var profileImage: ImageView
     private lateinit var fullNameEditText: EditText
     private lateinit var emailEditText: EditText
     private lateinit var phoneEditText: EditText
     private lateinit var saveButton: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -63,5 +83,25 @@ class ProfileFragment : Fragment() {
 
         // Show a confirmation message
         Toast.makeText(activity, "Profile saved", Toast.LENGTH_SHORT).show()
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment ProfileFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            ProfileFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
     }
 }
