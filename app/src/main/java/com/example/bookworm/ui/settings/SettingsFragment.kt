9be64ded17ee7d96ef93222bc049fed717976a64
NoaDolev <noa.dolev96@gmail.com>
2024-08-11 10:29:26 +0300

    Update the settingFragment with view options to see user data

--- app/src/main/java/com/example/bookworm/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/example/bookworm/ui/settings/SettingsFragment.kt
@@ -13,6 +13,13 @@ import com.example.bookworm.databinding.FragmentSettingsBinding
 import com.example.bookworm.ui.auth.AuthViewModel
 import com.google.firebase.auth.FirebaseAuth
 
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
+import com.example.bookworm.MainActivity
+import com.example.bookworm.databinding.FragmentRegisterBinding
+
 class SettingsFragment : Fragment() {
     private lateinit var binding: FragmentSettingsBinding
     private lateinit var viewModel: AuthViewModel
@@ -22,11 +29,18 @@ class SettingsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentSettingsBinding.inflate(inflater, container, false)
+        viewModel = ViewModelProvider(requireActivity()).get(AuthViewModel::class.java)
+
+        // Populate user data
+        val userName = viewModel.getUserName()
+        val userEmail = viewModel.getUserEmail()
+        binding.textView.text = userName
+        binding.textView2.text = userEmail
+
 
         //val firestoreDb: FirebaseFirestore = FirebaseFirestore.getInstance()
         val firestoreAuth: FirebaseAuth = FirebaseAuth.getInstance()
         //val userRepository = UserRepository(firestoreDb, firestoreAuth, UserDatabase.getDatabase(requireContext()).userDao())
-
 
         return binding.root
     }
