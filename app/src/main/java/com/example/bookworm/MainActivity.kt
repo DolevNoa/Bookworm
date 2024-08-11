3da1c47a50d75209fe0d71220cb96d2fc7123308
NoaDolev <noa.dolev96@gmail.com>
2024-08-11 16:27:39 +0300

    Add bottom navigation

--- app/src/main/java/com/example/bookworm/MainActivity.kt
+++ app/src/main/java/com/example/bookworm/MainActivity.kt
@@ -5,8 +5,17 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.example.bookworm.ui.settings.SettingsFragment
+import com.example.bookworm.ui.userProfile.ProfileFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +24,33 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottom_navigation)
+
+        NavigationUI.setupWithNavController(bottomNavigationView, navController)
+
+//        bottomNavigationView.setOnNavigationItemSelectedListener { item ->
+//            val fragment: Fragment = when (item.itemId) {
+//                R.id.nav_settings -> SettingsFragment.newInstance()
+////                R.id.nav_add_post -> AddPostFragment.newInstance()
+////                R.id.nav_user_posts -> UserPostsFragment.newInstance()
+////                R.id.nav_feed -> FeedFragment.newInstance()
+//                else -> throw IllegalArgumentException("Unexpected item ID")
+//            }
+//            supportFragmentManager.beginTransaction()
+//                .replace(R.id.nav_host_fragment, fragment)
+//                .commit()
+//            true
+//        }
+//
+//        // Set default fragment
+        if (savedInstanceState == null) {
+            bottomNavigationView.selectedItemId = R.id.settingsFragment
         }
     }
 }
