b6f569f5634084b93807ff48084eb4501676ccec
NoaDolev <noa.dolev96@gmail.com>
2024-08-13 21:51:28 +0300

    Add timestamp and creator to BookRecommendation.kt

--- app/src/main/java/com/example/bookworm/data/books/BookRecommendation.kt
+++ app/src/main/java/com/example/bookworm/data/books/BookRecommendation.kt
@@ -1,6 +1,10 @@
 package com.example.bookworm.data.books
 
+import com.google.firebase.Timestamp
+
 data class BookRecommendation(
+    val timestamp: Timestamp,
+    val creator: String,
     val bookName: String,
     val description: String,
     val rating: Float,
