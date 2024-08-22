cba8fb7483a77051db04a955f61aa966d999ea60
SageeKr <sageephone@gmail.com>
2024-08-22 19:16:04 +0300

    -made use of the ProgressBar in the fragment_book_list.xml to be shown as data is loaded.

--- app/src/main/java/com/example/bookworm/ui/books/BaseBookListFragment.kt
+++ app/src/main/java/com/example/bookworm/ui/books/BaseBookListFragment.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
@@ -29,6 +30,7 @@ abstract class BaseBookListFragment : Fragment() {
     protected lateinit var recyclerView: RecyclerView
     protected lateinit var feedAdapter: FeedAdapter
     protected lateinit var currentUserId: String
+    protected lateinit var progressBar: ProgressBar
     protected val viewModel: HandleBooksViewModel by activityViewModels()
 
     abstract fun fetchBookRecommendations()
@@ -38,7 +40,7 @@ abstract class BaseBookListFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_book_list, container, false)
-
+        progressBar = view.findViewById(R.id.paginationProgressBar)
         recyclerView = view.findViewById(R.id.recyclerView)
         recyclerView.layoutManager = LinearLayoutManager(context)
 
@@ -74,10 +76,12 @@ abstract class BaseBookListFragment : Fragment() {
 
     protected fun updateAdapter(bookRecommendations: List<BookRecommendation>, userProfiles: Map<String, UserProfile>) {
         feedAdapter.updateData(bookRecommendations, userProfiles)
+        progressBar.visibility = View.GONE
     }
 
     protected fun fetchRecommendationsFromViewModel(fetchMethod: suspend () -> List<BookRecommendation>) {
         CoroutineScope(Dispatchers.IO).launch {
+            progressBar.visibility = View.VISIBLE
             try {
                 val bookRecommendations = fetchMethod()
                 val userProfiles = fetchUserProfiles(bookRecommendations.map { it.creator }.distinct())
